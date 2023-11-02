53ccfb5d0269f7b83cc92ae596625cde1032811d
nidevelop <tx1009387@gmail.com>
2023-11-02 22:24:29 +0900

    s

--- setting.js
+++ setting.js
@@ -21,3 +21,36 @@
     ],
 }
 */
+
+/* vscode text coustomize
+{
+  "editor.tokenColorCustomizations": {
+    "textMateRules": [
+      {
+        "scope": ["entity.name.tag.tsx", "keyword.control.default.tsx", "keyword.control"],
+        "settings": {
+          "foreground": "#E06C75" // 태그 이름 색상
+        }
+      },
+      {
+        "scope": ["punctuation.definition.tag.end", "punctuation.definition.tag.begin"],
+        "settings": {
+          "foreground": "#ABB2BF" // 태그 열림 닫힘 꺽쇠
+        }
+      },
+      {
+        "scope": ["string.quoted.double", "string.quoted.single"],
+        "settings": {
+          "foreground": "#c5a05b" // 싱글 쿼터, 더블 쿼터
+        }
+      },
+      {
+        "scope": ["entity.other.attribute-name", "variable.other.property", "variable.parameter", "variable.other.readwrite.alias"],
+        "settings": {
+          "foreground": "#24d0e7" // 어트리뷰트, 프로퍼티, 파라미터
+        }
+      }
+    ]
+  }
+}
+*/
